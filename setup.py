be5bea799ac45ae8269ba9300caceb7359ff3a2d
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-04-11 12:17:38 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data = True,
+    name = 'calc2243336',
+    version = '0.0.1',
+    description = 'calc example for Pypi',
+    author = 'SangKyunJeon',
+    author_email = 'sangkyun.jeon@gmail.com',
+    url = 'https://github.com/jsk0910/calc2243336',
+    download_url = 'https://github.com/jsk0910/calc2243336/archive/refs/tags/v.0.0.1.zip',
+    install_requires = ['pytest'],
+    long_description = 'oss-dev calculator example',
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operatig System :: OS Independent"
+    ]
+)
